0662462e455b32282b9d134586c219564b459a82
codythompson <cody.thompson22@gmail.com>
2025-11-12 12:56:07 -0800

    more fixes to trackable types

--- src/trackable.ts
+++ src/trackable.ts
@@ -4,4 +4,4 @@ export interface Trackable {
 
 export type Tracked<T extends Trackable = Trackable> = T & {id: number}
 
-export type UnTracked<T extends Tracked = Tracked> = Omit<T, "id"> | Trackable
+export type UnTracked<T extends Trackable = Trackable> = Omit<T, "id"> & Trackable
